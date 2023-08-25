733e0da3aee9012106d92ca7818a363ec20889b5
Sean Burtenshaw <seanburtenshaw@Seans-MacBook-Pro.local>
2023-08-25 12:21:15 +0100

    Configured flowbite component library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "media",
+  plugins: [require("flowbite/plugin")],
 } satisfies Config;
