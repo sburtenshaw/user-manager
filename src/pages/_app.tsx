3af7ca1fa16243dd69da76e4241722973ed707e3
Sean Burtenshaw <seanburtenshaw@Seans-MacBook-Pro.local>
2023-08-25 12:37:47 +0100

    Apply dark mode by default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { type AppType } from "next/app";
+
+import { useEffect } from "react";
+
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    document.body.classList.add("dark");
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,6 @@ export default {
   theme: {
     extend: {},
   },
-  darkMode: "media",
+  darkMode: "class",
   plugins: [require("flowbite/plugin")],
 } satisfies Config;
