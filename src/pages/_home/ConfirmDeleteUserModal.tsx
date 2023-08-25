d1293020c7d248f2921b3365151498af60c79a41
Sean Burtenshaw <seanburtenshaw@Seans-MacBook-Pro.local>
2023-08-25 12:58:21 +0100

    Added confirm delete user modal

---
+++ src/pages/_home/ConfirmDeleteUserModal.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+
+import { Spinner, Button, Tooltip, Modal } from "flowbite-react";
+
+import { MdDelete } from "react-icons/md";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+
+import { type UserWithIdType } from "~/types";
+
+import { api } from "~/utils/api";
+
+interface ConfirmDeleteUserModalProps {
+  user: UserWithIdType;
+}
+
+function ConfirmDeleteUserModal({ user }: ConfirmDeleteUserModalProps) {
+  const apiContext = api.useContext();
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const deleteUser = api.user.deleteUser.useMutation({
+    onSuccess: () => {
+      void apiContext.user.invalidate();
+    },
+  });
+
+  const handleOpenModal = (): void => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = (): void => {
+    setModalOpen(false);
+  };
+
+  const handleDelete = (): void => {
+    deleteUser.mutate({ id: user.id });
+  };
+
+  const renderActions = () => {
+    if (deleteUser.isLoading) {
+      return <Spinner size="lg" />;
+    }
+    return (
+      <>
+        <Button color="gray" onClick={handleCloseModal}>
+          No, cancel
+        </Button>
+        <Button color="failure" onClick={handleDelete}>
+          Yes, I&apos;m sure
+        </Button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Tooltip content="Delete User">
+        <MdDelete
+          onClick={handleOpenModal}
+          className="h-5 w-5 cursor-pointer"
+        />
+      </Tooltip>
+      <Modal show={modalOpen} size="md" popup onClose={handleCloseModal}>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this user?
+            </h3>
+            <div className="flex justify-center gap-4">{renderActions()}</div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default ConfirmDeleteUserModal;
--- src/pages/_home/UsersTable.tsx
+++ src/pages/_home/UsersTable.tsx
@@ -1,44 +1,14 @@
 import { type UserWithIdType } from "~/types";
 
-import { Spinner, Tooltip, Table } from "flowbite-react";
+import { Table } from "flowbite-react";
 
-import { MdDelete } from "react-icons/md";
-
-import { api } from "~/utils/api";
+import ConfirmDeleteUserModal from "./ConfirmDeleteUserModal";
 
 interface UsersProps {
   users: UserWithIdType[];
 }
 
 function UsersTable({ users = [] }: UsersProps) {
-  const apiContext = api.useContext();
-
-  const deleteUser = api.user.deleteUser.useMutation({
-    onSuccess: () => {
-      void apiContext.user.invalidate();
-    },
-  });
-
-  const handleDeleteUser = (id: string): void => {
-    deleteUser.mutate({ id });
-  };
-
-  const renderDelete = (id: string) => {
-    if (deleteUser.isLoading && deleteUser.variables?.id === id) {
-      return <Spinner size="sm" />;
-    }
-    return (
-      <Tooltip content="Delete User">
-        <MdDelete
-          onClick={() => {
-            handleDeleteUser(id);
-          }}
-          className="h-5 w-5 cursor-pointer"
-        />
-      </Tooltip>
-    );
-  };
-
   if (!users.length) {
     return (
       <div className="rounded-lg py-10 text-center text-lg dark:border-gray-700 dark:bg-gray-800 dark:text-slate-200">
@@ -54,7 +24,6 @@ function UsersTable({ users = [] }: UsersProps) {
         <Table.HeadCell>Last name</Table.HeadCell>
         <Table.HeadCell>Email Address</Table.HeadCell>
         <Table.HeadCell>Favourite Food</Table.HeadCell>
-        {/* <Table.HeadCell>Edit</Table.HeadCell> */}
         <Table.HeadCell>Delete</Table.HeadCell>
       </Table.Head>
       <Table.Body className="divide-y">
@@ -67,7 +36,9 @@ function UsersTable({ users = [] }: UsersProps) {
             <Table.Cell>{user.lastName}</Table.Cell>
             <Table.Cell>{user.emailAddress}</Table.Cell>
             <Table.Cell>{user.favouriteFood}</Table.Cell>
-            <Table.Cell>{renderDelete(user.id)}</Table.Cell>
+            <Table.Cell>
+              <ConfirmDeleteUserModal user={user} />
+            </Table.Cell>
           </Table.Row>
         ))}
       </Table.Body>
